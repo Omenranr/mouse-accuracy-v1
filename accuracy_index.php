14abe071d83e7d78f0f13604e46425c0a749552d
Dazomen <omen-retro@hotmail.fr>
2018-05-20 16:46:37 +0000

    remove jquery migrate

--- accuracy_index.php
+++ accuracy_index.php
@@ -6,7 +6,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.10/addons/p5.dom.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="http://mouseaccuracy.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1"></script>
     <link rel="stylesheet" type="text/css" href="styles.css">
     <style>
         body {
